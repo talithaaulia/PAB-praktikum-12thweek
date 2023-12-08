dc61c3cfcef6a73465e54278b1bb23adfdab0e7d
talithaaulia <talithaali88@gmail.com>
2023-12-08 23:53:37 +0700

    note-app with firebase

--- src/config/FIREBASE/index.js
+++ src/config/FIREBASE/index.js
@@ -4,8 +4,13 @@ import "firebase/compat/firestore";
 import "firebase/compat/database";
 
 firebase.initializeApp({
-  // COPAS CONFIG DARI FIREBASE 
-  // MULAI  apiKey SAMPE appId
+  apiKey: "AIzaSyDWfnem6UdUXQ6CJlgKJ49ANfd_An6nENQ",
+  authDomain: "th-week-644cf.firebaseapp.com",
+  databaseURL: "https://th-week-644cf-default-rtdb.firebaseio.com",
+  projectId: "th-week-644cf",
+  storageBucket: "th-week-644cf.appspot.com",
+  messagingSenderId: "65027537062",
+  appId: "1:65027537062:web:f6bda23b47bc90e1f75c67"
 });
 
 const FIREBASE = firebase;
